220e6fe5a09cb0bd798a5639bcd532e94efc902b
Dan Bruder <danb@structionsite.com>
2021-07-22 09:31:37 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// load all files in a dir recursively
+// create a description of them
+// do that in two places
+// create a diff plan
+// execute that and see if it works
